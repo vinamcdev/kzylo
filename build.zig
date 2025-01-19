afa1e970b2573a36d8661613ce5b995703840795
vinamcdev <vinamdev@proton.me>
2025-01-19 12:40:14 +0000

    build(kernel): Remove grub and use UEFI to boot instead of mb2

--- build.zig
+++ build.zig
@@ -31,9 +31,18 @@ const kernel_config = .{
 pub fn build(b: *std.Build) void {
     const feature_mod = getFeatureMod(kernel_config.arch);
 
+    const args = b.args;
+
+    if (args) |argv| {
+        for (argv) |arg| {
+            std.debug.print("{s} ", .{arg});
+        }
+        std.debug.print("\n", .{});
+    }
+
     const target_query: std.Target.Query = .{
         .cpu_arch = kernel_config.arch,
-        .os_tag = .freestanding,
+        .os_tag = .uefi,
         .abi = .none,
         .cpu_features_add = feature_mod.add,
         .cpu_features_sub = feature_mod.sub,
@@ -50,38 +59,10 @@ pub fn build(b: *std.Build) void {
         .optimize = kernel_optimize,
     });
 
-    const kernel_file = "bin/kzylo.bin";
-    const bin_Dir = try std.fs.cwd().openDir("bin/", .{});
-    defer bin_Dir.close();
-
     kernel.setLinkerScript(.{ .cwd_relative = "linker.ld" });
-    b.addInstallArtifact(kernel, .{ .dest_dir = bin_Dir });
+    _ = b.installArtifact(kernel);
 
-    const iso_dir = "iso";
-    const iso_kernel_path = iso_dir ++ "/boot/kzylo.bin";
-    const copy_to_iso = b.addSystemCommand(&[_][]const u8{
-        "cp",
-        kernel_file,
-        iso_kernel_path,
-    });
-    copy_to_iso.step.dependOn(&kernel.step);
+    b.default_step.dependOn(&kernel.step);
 
-    const iso_file = "kzylo.iso";
-    const iso_command = b.addSystemCommand(&[_][]const u8{
-        "grub-mkrescue",
-        "-o",
-        iso_file,
-        iso_dir,
-    });
-    iso_command.step.dependOn(&copy_to_iso.step);
-
-    const run_qemu_command = b.addSystemCommand(&[_][]const u8{
-        "qemu-system-x86_64",
-        "-kernel",
-        kernel_file,
-        "-nographic",
-    });
-    run_qemu_command.step.dependOn(&kernel.step);
-
-    b.default_step.dependOn(&iso_command.step);
+    std.debug.print("[{s}OK{s}] Kernel build finished\n", .{ "\x1b[32;40m", "\x1b[39;49m" });
 }
