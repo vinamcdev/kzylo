ac0b5ccf0fb574e636bd28c728b04975826f8d45
vinamcdev <vinamdev@proton.me>
2025-01-21 21:07:16 +0000

    fix(output): Fixed []u8 to *[1:0]u16 conversion errors

--- src/include/string.zig
+++ src/include/string.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
 const fmt = @import("std").fmt;
-const global = @import("../constants.zig");
+const global = @import("../global.zig");
 
 fn puts(msg: []const u8) void {
     for (msg) |c| {
         const c_ = [2]u16{ c, 0 };
-        _ = global.con_out.outputString(@as(*const [1:0]u16, &c_));
+        const chr: *const [1:0]u16 = @ptrCast(&c_);
+        _ = global.con_out.outputString(chr);
     }
 }
 
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const uefi = @import("std").os.uefi;
 const string = @import("include/string.zig");
-const global = @import("constants.zig");
+const global = @import("global.zig");
 
 pub fn main() uefi.Status {
     global.init();
@@ -11,7 +11,7 @@ pub fn main() uefi.Status {
     var buf: [256]u8 = undefined;
     string.printf(buf[0..], "done\n", .{});
 
-    _ = global.boot_services.?.stall(3_000_000);
+    _ = global.boot_services.stall(3_000_000);
 
     return uefi.Status.Success;
 }
