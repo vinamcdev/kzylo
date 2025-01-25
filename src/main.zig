81230e9c77b83b3e4b7ff681f063e0a1dacde79c
vinamcdev <vinamdev@proton.me>
2025-01-25 16:18:05 +0000

    refactor(main): remove printing (was only there for debugging)

--- src/main.zig
+++ src/main.zig
@@ -5,9 +5,6 @@ pub fn main() uefi.Status {
     _ = uefi.system_table.con_out.?.reset(false);
     _ = uefi.system_table.con_out.?.clearScreen();
 
-    var buf: [256]u8 = undefined;
-    string.printf(buf[0..], "done\n", .{});
-
     _ = uefi.system_table.boot_services.?.stall(3_000_000);
 
     return uefi.Status.Success;
